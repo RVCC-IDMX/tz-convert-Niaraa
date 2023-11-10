d1288f8a01fab89eaa9dd3f27b0d4fea2427c06f
Niara <123399425+Niaraa@users.noreply.github.com>
2023-11-09 21:14:45 -0500

    edit README.md

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ESLint Configuration
+In this app I'm showing the time zone I am currently in and the time zone in another country at the exact same time. When entering the country and city in the app it will automatically convert the time to that place and show you what the time is in that time zone.
--- tz.js
+++ tz.js
@@ -2,6 +2,10 @@ const moment = require('moment-timezone');
 
 moment.tz.setDefault('America/New_York');
 
-const targetTimezone = 'Europe/Paris';
-
-console.log('The time at the ${targetTimezone} timezone is ${moment().tz(targetTimezone).format()}');
+if (process.argv.length !== 3) {
+  console.log('Usage: node <script-file> <timezone>');
+  process.exit(1);
+} else {
+  const targetTimezone = 'Europe/Paris';
+}
+console.log(`The time at the${targetTimezone} timezone is ${moment().tz(targetTimezone).format()}`);
